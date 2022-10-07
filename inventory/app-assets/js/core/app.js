0b60c06fc9db347b090b609cfc432e2c83036584
Aung Ko Man <aungkoman@gmail.com>
2022-10-07 20:38:37 +0630

    table added

--- inventory/app-assets/js/core/app.js
+++ inventory/app-assets/js/core/app.js
@@ -34,7 +34,7 @@ window.colors = {
   var $html = $('html');
   var $body = $('body');
   var $textcolor = '#4e5154';
-  var assetPath = '../../../app-assets/';
+  var assetPath = './app-assets/';
 
   if ($('body').attr('data-framework') === 'laravel') {
     assetPath = $('body').attr('data-asset-path');
